12e11079982718383df631ffdab56d3aa7fd81fe
Mason Maag <Masonmaag171@gmail.com>
2022-05-19 17:48:00 -0600

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 // console.log('hello world')
 
-const addMovie = (evt) => {
-    evt.preventDefault()
-    const inputField = document.querySelector("input")
-    const movie = document.createElement("li")
-    const movieTitle = document.createElement('span')
-    movieTitle.textContent = inputField.value
+document.querySelector("form").addEventListener("submit", addMovie);
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+  event.preventDefault();
+  let inputField = document.querySelector('input')
+
+  const movie = document.createElement('li')
+
+  const movieTitle = document.createElement("span");
+  
+  movieTitle.textContent = inputField.value;
+  movieTitle.addEventListener("click", crossOffMovie);
+  movie.appendChild(movieTitle)
+
+  const deleteBtn = document.createElement("button");
+  
+  deleteBtn.textContent = "x";
+  deleteBtn.addEventListener("click", deleteMovie);
+  movie.appendChild(deleteBtn);
+
+  const list = document.querySelector("ul");
+  list.appendChild(movie);
+
+  inputField.value = ''
+}
+
+function deleteMovie(event) {
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+    message.textContent = `Movie deleted!`
+
+    revealMessage()
+
+    event.target.parentNode.remove();
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked') === true) {
+        message.textContent = 'Movie watched!'
+        message.textContent = `${event.target.textContent} watched!`
+    } else {
+        message.textContent = 'Movie added back!'
+        message.textContent = `${event.target.textContent} added back!`
+    }
+
+    revealMessage()
+}
+
+function revealMessage() {
+    message.classList.remove('hide')
     
-    movie.appendChild(movieTitle)
-   
-
-    
-
-    const deleteBtn = document.createElement('button')
-    deleteBtn.textContent =("X")
-    movie.appendChild(deleteBtn)
-    document.querySelector('ul').appendChild(movie)
-    deleteBtn.addEventListener('click', deleteMovie)
-    movie.appendChild(deleteBtn)
-    inputField.value = ''
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
 }
-const crossOffMovie = (evt) => {
-    evt.target.classList.toggle(".checked")
-}
-
-const deleteMovie = (evt) => {
-    evt.target.parentNode.remove()
-}
-
-document.querySelector('form').addEventListener('submit',addMovie)
